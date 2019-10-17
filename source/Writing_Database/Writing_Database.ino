5a06cbab9cd658d36467da488d766e31e0164e12
STOERIMIAS <55968422+STOERIMIAS@users.noreply.github.com>
2019-10-17 09:54:38 +0800

    Config update

--- source/Writing_Database/Writing_Database.ino
+++ source/Writing_Database/Writing_Database.ino
@@ -41,14 +41,14 @@ const char* password = "getstuffdone";
 
 const String config[DEVICE_COUNT][TABLE_WIDTH]
 {
-    //MAC Address          location          room            type
-    {"b8:77:10:c2:dd:bc", "building9",      "fusebox",      "power"  },
-    {"44:39:69:3a:7d:80", "building1",      "dummy",        "power"  },
-    {"e8:42:69:3a:7d:80", "coderbunker",    "artoffice",    "power"  },
-    {"cd:36:69:3a:7d:80", "coderbunker",    "kitchen",      "quality"},
-    {"3d:78:10:c2:dd:bc", "coderbunker",    "artoffice",    "quality"},
-    {"fb:61:69:3a:7d:80", "coderbunker",    "classroom",    "quality"},
-    {"49:7a:10:c2:dd:bc", "coderbunker",    "meetingroom",  "quality"}
+  //MAC Address          location          room            type
+  {"b8:77:10:c2:dd:bc", "building9",      "kitchen",      "quality"},
+  {"44:39:69:3a:7d:80", "building1",      "dummy",        "power"  },
+  {"e8:42:69:3a:7d:80", "coderbunker",    "artoffice",    "power"  },
+  {"cd:36:69:3a:7d:80", "coderbunker",    "fusebox",      "power"  },
+  {"3d:78:10:c2:dd:bc", "coderbunker",    "artoffice",    "quality"},
+  {"fb:61:69:3a:7d:80", "coderbunker",    "classroom",    "quality"},
+  {"49:7a:10:c2:dd:bc", "coderbunker",    "meetingroom",  "quality"}
 };
 
 String  nameLocation = "";
@@ -77,16 +77,16 @@ InfluxDB_Data   dataAirQuality("quality_measurement");
 
 //Ticker stuff -- START -------------------------------------------------------
 Ticker  DB_Updater;
-int Timer=0;
+int Timer = 0;
 bool tickOccured = false;
 
 void timerCallback() {
-  if(Timer >= 50){
-      tickOccured = true;
-      Timer=0;
+  if (Timer >= 50) {
+    tickOccured = true;
+    Timer = 0;
   }
-  else{
-      Timer++;
+  else {
+    Timer++;
   }
 }
 //Ticker stuff -- UDP ---------------------------------------------------------
@@ -103,8 +103,8 @@ const float VOLTAGE_MAINS   = 220.00;   // line voltage
 const float COIL_WINDING    = 2000;     // ratio of sensor 100:0.05
 
 //int16_t rawADCvalue;  // The is where we store the value we receive from the ADS1115
-//float MAX_CURRENT_COIL = 0.0707; // Maximum current of sensor rated at  Rms 100 A 
-//float MAX_VOLTAGE_ADC = 15.981; // Maximum Voltage on burden resistor calculated by burden resistor * maxAmps (33 Ohm * 0.0707 A)
+float MAX_CURRENT_COIL = 0.0707; // Maximum current of sensor rated at  Rms 100 A
+float MAX_VOLTAGE_ADC = 15.981; // Maximum Voltage on burden resistor calculated by burden resistor * maxAmps (33 Ohm * 0.0707 A)
 
 float voltage_adc_1 = 0.0; // The result of applying the scale factor to the raw value
 float voltage_adc_2 = 0.0;
@@ -124,291 +124,292 @@ float eCO2 = 0;
 float TVOC = 0;
 float eCO2OLD = 0;
 float TVOCOLD = 0;
-float LongRST=0;
+float LongRST = 0;
 
-int writeflag =0;
+int writeflag = 0;
 
 String line;
 String PowerAsString;
 
 
-int n=0;
+int n = 0;
 
 
 void SWI(void)
 {
-   writeflag=0;
-   digitalWrite(D5,LOW);                                    //Reset Sensor 
-   delay(100);
-   digitalWrite(D5,HIGH);  
-   Serial.println("Reset Sensor"); 
-   CCS811.begin();
-   delay(5000);
-   while(eCO2 == 0.00){
-     eCO2=CCS811.geteCO2();
-     delay(100);
-   }
+  writeflag = 0;
+  digitalWrite(D5, LOW);                                   //Reset Sensor
+  delay(100);
+  digitalWrite(D5, HIGH);
+  Serial.println("Reset Sensor");
+  CCS811.begin();
+  delay(5000);
+  while (eCO2 == 0.00) {
+    eCO2 = CCS811.geteCO2();
+    delay(100);
+  }
   Serial.println("Sensor ready");
 }
 
 
 void reconnectWifi(void)
 {
-    WiFi.softAPdisconnect (true);
-    static int n = 0;
-    n=0;
-    WiFi.begin(ssid, password);
-    
-    while (WiFi.status() != WL_CONNECTED || n > 100) {
-        delay(500);
-        Serial.print("."); 
-        n++;
-    }
-    Serial.print("reconnected");
+  WiFi.softAPdisconnect (true);
+  static int n = 0;
+  n = 0;
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED || n > 100) {
+    delay(500);
+    Serial.print(".");
+    n++;
+  }
+  Serial.print("reconnected");
 }
 void setup(void)
 {
-    /*-------------------Software Interupt-----------------------------------*/
-    pinMode(D5,OUTPUT);
-    digitalWrite(D5,HIGH);
-    //-----------------------------------------------------------------------//
-    //---------------------------Wifihotspo--from--ESP--disable---------------
-    WiFi.softAPdisconnect (true);
-    Wire.setClock(400000);
-    Serial.begin(115200);                        
-    delay(1000);
-  
-    // WiFi Connection -- START -----------------------------------------------
-    WiFi.begin(ssid, password);
-    
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("Wifi Connected to " + String(ssid));
-    // WiFi Connection -- END -------------------------------------------------
+  /*-------------------Software Interupt-----------------------------------*/
+  pinMode(D5, OUTPUT);
+  digitalWrite(D5, HIGH);
+  //-----------------------------------------------------------------------//
+  //---------------------------Wifihotspo--from--ESP--disable---------------
+  WiFi.softAPdisconnect (true);
+  Wire.setClock(400000);
+  Serial.begin(115200);
+  delay(1000);
 
-    // WiFi Mac Address Mapping -- START --------------------------------------
-    uint8_t mac[6];
-    WiFi.macAddress(mac);
+  // WiFi Connection -- START -----------------------------------------------
+  WiFi.begin(ssid, password);
 
-    String mac_address = MacToString(mac);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Wifi Connected to " + String(ssid));
+  // WiFi Connection -- END -------------------------------------------------
 
-    nameLocation = getConfigValue(mac_address, INDEX_LOCATION);
-    nameRoom     = getConfigValue(mac_address, INDEX_ROOM);
-    
-    Serial.println("\n----- IMPORTANT INFORMATION -----");
-    Serial.println(mac_address);
-    Serial.println(nameLocation);
-    Serial.println(nameRoom);
-    Serial.println("---------------------------------\n");
+  // WiFi Mac Address Mapping -- START --------------------------------------
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
 
-    if(getConfigValue(mac_address, INDEX_TYPE) == "power")
+  String mac_address = MacToString(mac);
+
+  nameLocation = getConfigValue(mac_address, INDEX_LOCATION);
+  nameRoom     = getConfigValue(mac_address, INDEX_ROOM);
+
+  Serial.println("\n----- IMPORTANT INFORMATION -----");
+  Serial.println(mac_address);
+  Serial.println(nameLocation);
+  Serial.println(nameRoom);
+  Serial.println("---------------------------------\n");
+
+  if (getConfigValue(mac_address, INDEX_TYPE) == "power")
+  {
+    isPowerType = 1;
+    isQualityType = 0;
+    Serial.println("power");
+
+    dataPower.addTag("location", nameLocation);
+    dataPower.addTag("room", nameRoom);
+
+  }
+  else
+  {
+    isPowerType = 0;
+    isQualityType = 1;
+
+    dataAirQuality.addTag("location", nameLocation);
+    dataAirQuality.addTag("room", nameRoom);
+  }
+  // WiFi Mac Address Mapping -- END ----------------------------------------
+
+  // Sensor Initialisation -- START -----------------------------------------
+  if (isQualityType)
+  {
+    Si7021.begin();
+    CCS811.begin();
+
+    if (!CCS811.begin())
     {
-        isPowerType = 1;
-        isQualityType = 0;
-
-        dataPower.addTag("location",nameLocation);
-        dataPower.addTag("room",nameRoom);
-        
+      Serial.println("Failed to start CCS811! Please check your wiring.");
+      while (!CCS811.available())
+      {
+        delay(200);
+      }
     }
     else
     {
-        isPowerType = 0;
-        isQualityType = 1;
-
-        dataAirQuality.addTag("location",nameLocation);
-        dataAirQuality.addTag("room",nameRoom);
+      Serial.println("CCS811 found");
     }
-    // WiFi Mac Address Mapping -- END ----------------------------------------
-
-    // Sensor Initialisation -- START -----------------------------------------
-    if(isQualityType)
-    {        
-        Si7021.begin();
-        CCS811.begin();
-        
-        if(!CCS811.begin())
-        {
-            Serial.println("Failed to start CCS811! Please check your wiring.");
-            while(!CCS811.available())
-            {
-                delay(200);
-            }
-        }
-        else
-        {
-            Serial.println("CCS811 found");
-        }
-        while(!CCS811.available())
-        {
-            delay(200);
-        }
-        
-        float temp;
-        temp = CCS811.calculateTemperature();
-        CCS811.setTempOffset(temp - 25.0);
-    }
-    
-    if(isPowerType)
+    while (!CCS811.available())
     {
-        ADC_1.setGain(GAIN_ONE);  
-        ADC_2.setGain(GAIN_ONE);
-        
-        ADC_1.begin();
-        ADC_2.begin();
+      delay(200);
     }
 
-    // Sensor Initialisation -- END -------------------------------------------
+    float temp;
+    temp = CCS811.calculateTemperature();
+    CCS811.setTempOffset(temp - 25.0);
+  }
 
-    //activate Ticker
-    DB_Updater.attach(1, timerCallback);
+  if (isPowerType)
+  {
+    ADC_1.setGain(GAIN_ONE);
+    ADC_2.setGain(GAIN_ONE);
+
+    ADC_1.begin();
+    ADC_2.begin();
+  }
+
+  // Sensor Initialisation -- END -------------------------------------------
+
+  //activate Ticker
+  DB_Updater.attach(1, timerCallback);
 
 
-    }
-    
+}
+
 void loop(void)
-{   
-   if(WiFi.status() != WL_CONNECTED)
-    {
-      reconnectWifi();      
-    }
-    
-    if((tickOccured == true) && (writeflag == 1))
-    {
-        writeflag=0;
-        tickOccured = false;
-        if(isQualityType)
-        {
-            dataAirQuality.clear(InfluxDB_Data::FIELD);
-            dataAirQuality.addField("temperature_c",String(temperature));
-            dataAirQuality.addField("TVOC_ppb",String(TVOC));
-            dataAirQuality.addField("eCO2_ppm",String(eCO2));
-            dataAirQuality.addField("humidity_RH",String(humidity));
-            influxSensorPort.write(dataAirQuality);
-        }
-        if(isPowerType)
-        {
-            dataPower.clear(InfluxDB_Data::FIELD);
-            dataPower.addField("power1_W",String(power_1));
-            dataPower.addField("power2_W",String(power_2));
-            dataPower.addField("power3_W",String(power_3));
-            dataPower.addField("powerTotal_W",String(power_total));
-            influxPowerPort.write(dataPower);
-        } 
-    }
-    if(isQualityType)
-    {
-        LongRST++;
-        delay(100);
-        if(LongRST >=12000)
-        {
-            LongRST=0;
-            SWI();
-        }
-        humidity = Si7021.getRH();
-        temperature = Si7021.getTemp();
-        if(CCS811.available())
-        {
-            if(!CCS811.readData())
-            {
-                eCO2OLD=eCO2;
-                TVOCOLD=TVOC;
-                eCO2=CCS811.geteCO2();
-                TVOC=CCS811.getTVOC();
-            }
-            else
-            {
-                Serial.println("ERROR!");
-            }
-        }
-        if(eCO2-eCO2OLD >= 20)
-        {
-          SWI();      
-        }
-        else
-        {
-          writeflag = 1;
-        }
-    }
-    //  current_coil = ((voltage_adc_1) * MAX_CURRENT_COIL)/MAX_VOLTAGE_ADC;
-    //  power = current_coil * COIL_WINDING * VOLTAGE_MAINS;
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    reconnectWifi();
+  }
 
-    if(isPowerType)
+  if ((tickOccured == true) && (writeflag == 1))
+  {
+    writeflag = 0;
+    tickOccured = false;
+    if (isQualityType)
     {
-        voltage_adc_1 = (calcVrms(32,1) )/1000.0;
-        voltage_adc_2 = (calcVrms(32,2) )/1000.0;
-        voltage_adc_3 = (calcVrms(32,3) )/1000.0;
-
-        power_1= calcPower(voltage_adc_1); 
-        power_2= calcPower(voltage_adc_2);
-        power_3= calcPower(voltage_adc_3);
-        power_total=power_1+power_2+power_3;
+      dataAirQuality.clear(InfluxDB_Data::FIELD);
+      dataAirQuality.addField("temperature_c", String(temperature));
+      dataAirQuality.addField("TVOC_ppb", String(TVOC));
+      dataAirQuality.addField("eCO2_ppm", String(eCO2));
+      dataAirQuality.addField("humidity_RH", String(humidity));
+      influxSensorPort.write(dataAirQuality);
     }
+    if (isPowerType)
+    {
+      dataPower.clear(InfluxDB_Data::FIELD);
+      dataPower.addField("power1_W", String(power_1));
+      dataPower.addField("power2_W", String(power_2));
+      dataPower.addField("power3_W", String(power_3));
+      dataPower.addField("powerTotal_W", String(power_total));
+      influxPowerPort.write(dataPower);
+    }
+  }
+  if (isQualityType)
+  {
+    LongRST++;
+    delay(100);
+    if (LongRST >= 12000)
+    {
+      LongRST = 0;
+      SWI();
+    }
+    humidity = Si7021.getRH();
+    temperature = Si7021.getTemp();
+    if (CCS811.available())
+    {
+      if (!CCS811.readData())
+      {
+        eCO2OLD = eCO2;
+        TVOCOLD = TVOC;
+        eCO2 = CCS811.geteCO2();
+        TVOC = CCS811.getTVOC();
+      }
+      else
+      {
+        Serial.println("ERROR!");
+      }
+    }
+    if (eCO2 - eCO2OLD >= 100)
+    {
+      SWI();
+    }
+    else
+    {
+      writeflag = 1;
+    }
+  }
+  // current_coil = ((voltage_adc_1) * MAX_CURRENT_COIL)/MAX_VOLTAGE_ADC;
+  //power = current_coil * COIL_WINDING * VOLTAGE_MAINS;
+
+  if (isPowerType)
+  {
+    voltage_adc_1 = (calcVrms(32, 1) ) / 1000.0;
+    voltage_adc_2 = (calcVrms(32, 2) ) / 1000.0;
+    voltage_adc_3 = (calcVrms(32, 3) ) / 1000.0;
+
+    power_1 = calcPower(voltage_adc_1);
+    power_2 = calcPower(voltage_adc_2);
+    power_3 = calcPower(voltage_adc_3);
+    power_total = power_1 + power_2 + power_3;
+  }
 }
 
 double calcVrms(unsigned int Samples, unsigned int phase)
 {
-    float voltage_rms;
-    float squared_voltage;
-    float sample_voltage_sum;
-    float sample_voltage;
+  float voltage_rms;
+  float squared_voltage;
+  float sample_voltage_sum;
+  float sample_voltage;
 
-    for (unsigned int n = 0; n < Samples; n++)
-    {
-        switch(phase){
-            case 1:{
-                sample_voltage = ADC_1.readADC_Differential_0_1();
-                break;
-            }
-            case 2:{
-                sample_voltage = ADC_1.readADC_Differential_2_3();
-                break;
-            }
-            case 3:{
-                sample_voltage = ADC_2.readADC_Differential_0_1();
-                break;
-            }
-            default: break;
+  for (unsigned int n = 0; n < Samples; n++)
+  {
+    switch (phase) {
+      case 1: {
+          sample_voltage = ADC_1.readADC_Differential_0_1();
+          break;
         }
-        squared_voltage = sample_voltage * sample_voltage;
-        sample_voltage_sum += squared_voltage;
+      case 2: {
+          sample_voltage = ADC_1.readADC_Differential_2_3();
+          break;
+        }
+      case 3: {
+          sample_voltage = ADC_2.readADC_Differential_0_1();
+          break;
+        }
+      default: break;
     }
-    
-    voltage_rms = (sqrt(sample_voltage_sum / Samples) * SCALEFACTOR) * sqrt(2);
-    sample_voltage_sum = 0;    
-    return voltage_rms;
+    squared_voltage = sample_voltage * sample_voltage;
+    sample_voltage_sum += squared_voltage;
+  }
+
+  voltage_rms = (sqrt(sample_voltage_sum / Samples) * SCALEFACTOR) * sqrt(2);
+  sample_voltage_sum = 0;
+  return voltage_rms;
 }
 
 double calcPower(float voltage)
 {
-    double power;
-    power = voltage / BURDEN_RESISTOR * COIL_WINDING * VOLTAGE_MAINS;
-    return power;
+  double power;
+  power = voltage / BURDEN_RESISTOR * COIL_WINDING * VOLTAGE_MAINS;
+  return power;
 }
 
 String getConfigValue(String mac_address, int index)
 {
-    for(int index_counter = 0 ; index_counter < DEVICE_COUNT ; index_counter++)
+  for (int index_counter = 0 ; index_counter < DEVICE_COUNT ; index_counter++)
+  {
+    if (config[index_counter][INDEX_MAC] == mac_address)
     {
-        if(config[index_counter][INDEX_MAC] == mac_address)
-        {
-            return config[index_counter][index];
-        }
+      return config[index_counter][index];
     }
-    return "";
+  }
+  return "";
 }
 
 String MacToString(const uint8_t* mac)
 {
-    String s_mac = "";
-    for(int8_t index = 5 ; index >= 0 ; index--)
+  String s_mac = "";
+  for (int8_t index = 5 ; index >= 0 ; index--)
+  {
+    s_mac += String(mac[index], HEX);
+    if (index != 0)
     {
-        s_mac += String(mac[index],HEX);
-        if(index != 0)
-        {
-            s_mac += ":";
-        }
+      s_mac += ":";
     }
-    return s_mac;
+  }
+  return s_mac;
 }
